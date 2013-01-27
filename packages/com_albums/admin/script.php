118c2e9a2c900800513103e427149e5b6dc201f7
Bruno Batista <bruno@joomlapro.com.br>
2013-01-27 13:55:28 -0200

    Added script file in component

---
+++ packages/com_albums/admin/script.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @package     Albums
+ * @subpackage  com_albums
+ * @copyright   Copyright (C) 2013 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/**
+ * Script file of Albums Component.
+ *
+ * @package     Albums
+ * @subpackage  com_albums
+ * @since       3.0
+ */
+class com_albumsInstallerScript
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script.
+	 *
+	 * @return  void
+	 */
+	public function __constructor(JAdapterInstance $adapter)
+	{
+
+	}
+
+	/**
+	 * Called before any type of action.
+	 *
+	 * @param   string            $route    Which action is happening (install|uninstall|discover_install).
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   3.0
+	 */
+	public function preflight($route, JAdapterInstance $adapter)
+	{
+
+	}
+
+	/**
+	 * Called after any type of action.
+	 *
+	 * @param   string            $route    Which action is happening (install|uninstall|discover_install).
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script.
+	 *
+	 * @return  boolean  True on success
+	 *
+	 * @since   3.0
+	 */
+	public function postflight($route, JAdapterInstance $adapter)
+	{
+
+	}
+
+	/**
+	 * Called on installation.
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script.
+	 *
+	 * @return  boolean  True on success
+	 *
+	 * @since   3.0
+	 */
+	public function install(JAdapterInstance $adapter)
+	{
+
+	}
+
+	/**
+	 * Called on update.
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   3.0
+	 */
+	public function update(JAdapterInstance $adapter)
+	{
+
+	}
+
+	/**
+	 * Called on uninstallation.
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   3.0
+	 */
+	public function uninstall(JAdapterInstance $adapter)
+	{
+
+	}
+}
