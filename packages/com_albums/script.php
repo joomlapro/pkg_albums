74b642e7f84e09675a0e498e9070370751074b77
Bruno Batista <bruno@joomlapro.com.br>
2013-01-27 18:13:40 -0200

    Update script file

--- packages/com_albums/script.php
+++ packages/com_albums/script.php
@@ -57,7 +57,7 @@ class com_albumsInstallerScript
 	 */
 	public function postflight($route, JAdapterInstance $adapter)
 	{
-
+		$this->_addCategory();
 	}
 
 	/**
@@ -100,5 +100,58 @@ class com_albumsInstallerScript
 	public function uninstall(JAdapterInstance $adapter)
 	{
 
+	}
+
+	/**
+	 * Method to add a default category "uncategorised".
+	 *
+	 * @return  integer  Id of the created category.
+	 *
+	 * @since   3.0
+	 */
+	public function _addCategory()
+	{
+		// Create categories for our component.
+		$basePath = JPATH_ADMINISTRATOR . '/components/com_categories';
+
+		require_once $basePath . '/models/category.php';
+
+		$config   = array('table_path' => $basePath . '/tables');
+		$catmodel = new CategoriesModelCategory($config);
+
+		$catData  = array(
+			'id'          => 0,
+			'parent_id'   => 0,
+			'level'       => 1,
+			'path'        => 'uncategorised',
+			'extension'   => 'com_albums',
+			'title'       => 'Uncategorised',
+			'alias'       => 'uncategorised',
+			'description' => '',
+			'published'   => 1,
+			'params'      => '{"target":"","image":""}',
+			'metadata'    => '{"page_title":"","author":"","robots":""}',
+			'language'    => '*'
+		);
+
+		$catmodel->save($catData);
+		$id = $catmodel->getItem()->id;
+
+		$db = JFactory::getDBO();
+
+		// Updating all albums without category to have this new one.
+		$query = $db->getQuery(true);
+		$query->update('#__albums');
+		$query->set('catid = ' . (int) $id);
+		$query->where('catid = 0');
+		$db->setQuery($query);
+		$db->query();
+
+		// Updating all places without category to have this new one.
+		// $query->update('#__albums_places');
+		// $query->setQuery($query);
+		// $db->query();
+
+		return;
 	}
 }
