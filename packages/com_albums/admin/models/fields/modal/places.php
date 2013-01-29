0bafe73a57778fd720c8d18096ba382537492600
Bruno Batista <bruno@joomlapro.com.br>
2013-01-28 22:43:12 -0200

    Finished Album view

---
+++ packages/com_albums/admin/models/fields/modal/places.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * @package     Albums
+ * @subpackage  com_albums
+ * @copyright   Copyright (C) 2013 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('JPATH_BASE') or die;
+
+/**
+ * Supports a modal places picker.
+ *
+ * @package     Albums
+ * @subpackage  com_albums
+ * @since       3.0
+ */
+class JFormFieldModal_Places extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $type = 'Modal_Places';
+
+	/**
+	 * Method to get the field label markup.
+	 *
+	 * @return  string  The field label markup.
+	 *
+	 * @since   3.0
+	 */
+	protected function getLabel()
+	{
+		$label = '';
+
+		if ($this->hidden)
+		{
+			return $label;
+		}
+
+		// Get the label text from the XML element, defaulting to the element name.
+		$text = $this->element['label'] ? (string) $this->element['label'] : (string) $this->element['name'];
+		$text = $this->translateLabel ? JText::_($text) : $text;
+
+		// Build the class for the label.
+		$class = !empty($this->description) ? 'hasTip' : '';
+		$class = $this->required == true ? $class . ' required' : $class;
+		$class = !empty($this->labelClass) ? $class . ' ' . $this->labelClass : $class;
+
+		// Add the opening label tag and main attributes attributes.
+		$label .= '<label id="' . $this->id . '-lbl" for="' . $this->id . '_id" class="' . $class . '"';
+
+		// If a description is specified, use it to build a tooltip.
+		if (!empty($this->description))
+		{
+			$label .= ' title="'
+				. htmlspecialchars(
+				trim($text, ':') . '::' . ($this->translateDescription ? JText::_($this->description) : $this->description),
+				ENT_COMPAT, 'UTF-8'
+			) . '"';
+		}
+
+		// Add the label text and closing tag.
+		if ($this->required)
+		{
+			$label .= '>' . $text . '<span class="star">&#160;*</span></label>';
+		}
+		else
+		{
+			$label .= '>' . $text . '</label>';
+		}
+
+		return $label;
+	}
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   3.0
+	 */
+	protected function getInput()
+	{
+		// Load the modal behavior script.
+		JHtml::_('behavior.framework');
+		JHtml::_('behavior.modal', 'a.modal');
+		JHtml::_('bootstrap.tooltip');
+
+		// Build the script.
+		$script = array();
+		$script[] = '	function jSelectPlaces_' . $this->id . '(id, name, catid, object, link, lang) {';
+		$script[] = '		document.id("' . $this->id . '_id").value = id;';
+		$script[] = '		document.id("' . $this->id . '_name").value = name;';
+		$script[] = '		SqueezeBox.close();';
+		$script[] = '	}';
+
+		// Add the script to the document head.
+		JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+
+		// Setup variables for display.
+		$html = array();
+		$link = 'index.php?option=com_albums&amp;view=places&amp;layout=modal&amp;tmpl=component&amp;function=jSelectPlaces_' . $this->id;
+
+		if (isset($this->element['language']))
+		{
+			$link .= '&amp;forcedLanguage=' . $this->element['language'];
+		}
+
+		$db = JFactory::getDBO();
+		$db->setQuery(
+			'SELECT name' .
+			' FROM #__albums_places' .
+			' WHERE id = ' . (int) $this->value
+		);
+
+		try
+		{
+			$name = $db->loadResult();
+		}
+		catch (RuntimeException $e)
+		{
+			JError::raiseWarning(500, $e->getMessage());
+		}
+
+		if (empty($name))
+		{
+			$name = JText::_('COM_ALBUMS_SELECT_AN_PLACE');
+		}
+
+		$name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+
+		// The current user display field.
+		$html[] = '<span class="input-append">';
+		$html[] = '<input type="text" class="input-medium" id="' . $this->id . '_name" value="' . $name . '" disabled="disabled" size="35" /><a class="modal btn" title="' . JText::_('COM_ALBUMS_CHANGE_PLACE') . '" href="' . $link . '&amp;' . JSession::getFormToken() . '=1" rel="{handler: \'iframe\', size: {x: 800, y: 450}}"><i class="icon-file"></i> ' . JText::_('JSELECT') . '</a>';
+		$html[] = '</span>';
+
+		// The active places id field.
+		if (0 == (int) $this->value)
+		{
+			$value = '';
+		}
+		else
+		{
+			$value = (int) $this->value;
+		}
+
+		// The class='required' for client side validation.
+		$class = '';
+
+		if ($this->required)
+		{
+			$class = ' class="required modal-value"';
+		}
+
+		$html[] = '<input type="hidden" id="' . $this->id . '_id"' . $class . ' name="' . $this->name . '" value="' . $value . '" />';
+
+		return implode("\n", $html);
+	}
+}
---
+++ packages/com_albums/admin/models/fields/place.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package     Albums
+ * @subpackage  com_albums
+ * @copyright   Copyright (C) 2013 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('JPATH_BASE') or die;
+
+JFormHelper::loadFieldClass('list');
+
+/**
+ * Place Field class for the Albums.
+ *
+ * @package     Albums
+ * @subpackage  com_albums
+ * @since       3.0
+ */
+class JFormFieldPlace extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $type = 'Place';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 *
+	 * @since   3.0
+	 */
+	protected function getOptions()
+	{
+		// Initialiase variables.
+		$options = AlbumsHelper::getPlaceOptions();
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
+}
